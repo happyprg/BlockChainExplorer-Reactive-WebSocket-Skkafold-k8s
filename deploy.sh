04ed6b979df007377317bd37790e34a6b8d531b5
jeehong.lee@linecorp.com <jeehong.lee@linecorp.com>
2019-09-29 17:20:38 +0900

    demo

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ gradle assemble
 docker rmi -f hongsgo/blockexplorer:latest
 docker build -t hongsgo/blockexplorer:latest api/
 docker push hongsgo/blockexplorer:latest
-kubectl get po -l run=blockexplorer
-#skaffold run
+kubectl  delete -l run=blockexplorer
